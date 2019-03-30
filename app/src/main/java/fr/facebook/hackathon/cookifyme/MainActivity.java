1871b2b903617ab3c8c95bf31bba5e1b05dc613a
Yougourta <dy_ait_saada@esi.dz>
2019-03-30 18:23:52 +0100

    add initial ingredients view

--- app/src/main/java/fr/facebook/hackathon/cookifyme/MainActivity.java
+++ app/src/main/java/fr/facebook/hackathon/cookifyme/MainActivity.java
@@ -1,52 +1,48 @@
 package fr.facebook.hackathon.cookifyme;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final FragmentManager fm = getSupportFragmentManager();
+    private final Fragment ingredientsFm = new IngredientsFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        BottomNavigationView navigation = findViewById(R.id.btn_recruiter);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        fm.beginTransaction().add(R.id.main_frame, ingredientsFm).show(ingredientsFm).commit();
+    }
 
-       FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            int itemId = item.getItemId();
+            switch (itemId) {
+                case R.id.navigation_one:
+                    fm.beginTransaction().replace(R.id.main_frame, ingredientsFm).commit();
+                    return true;
+                case R.id.navigation_two:
+                    return true;
+                case R.id.navigation_three:
+                    return true;
+                default:
+                    break;
             }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            return false;
         }
-
-        return super.onOptionsItemSelected(item);
-    }
+    };
 }
